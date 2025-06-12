16708a42ad19cd7c5106fdd879e1eeb88d240a37
Esteban Sánchez <estebanasp8@gmail.com>
2025-06-12 16:26:37 -0500

    Agregar archivos de proyecto.

---
+++ NoticiasAPI/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NoticiasAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+    {
+    }
+}
---
+++ NoticiasAPI/Entities/Noticia.cs
@@ -0,0 +1,12 @@
+﻿namespace NoticiasAPI.Entities
+{
+    public class Noticia
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public string Autor { get; set; }
+        public string Categoria { get; set; }
+    }
+}
